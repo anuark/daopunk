b58ec54fa63a253827aaf192dc85a1ae64fb56fe
Anuar <anuar.kilgore@buyersedgeplatform.com>
2021-11-27 22:33:46 -0600

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { Button, Form, Row, Container, Navbar } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [date, setDate] = useState(null);
@@ -12,46 +14,34 @@ function App() {
     }
     getDate();
   }, []);
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <Row>
+      <h1>Title 3</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="dao-name">
+          <Form.Label>DAO Name</Form.Label>
+          <Form.Control type="text" placeholder="Constitution DAO..." />
+          {/*<Form.Text className="text-muted">
+          </Form.Text> */}
+        </Form.Group>
+      
+        <Form.Group className="mb-3" controlId="dao-token">
+          <Form.Label>Token Name</Form.Label>
+          <Form.Control type="text" placeholder="Token" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Quadratic Voting" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Row>
   );
 }
 
 export default App;
+
