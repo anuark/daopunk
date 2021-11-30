12869fc637a42939a10b3701decc58c5aa86d46e
Djaydurden <dhananjay.durden@gmail.com>
2021-11-30 15:09:51 -0500

    Metamask

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Row, Container, Navbar } from 'react-bootstrap';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import OnboardingButton from './metamask'
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from './assets/logo_size.png';
@@ -8,7 +10,10 @@ import Logo from './assets/logo_size.png';
 function App() {
   console.log('App.js is running');
   return (
-    <h1> Hello World </h1>
+   // <h1> Hello World </h1>
+    <Router>
+        <OnboardingButton/>
+    </Router>
   );
 }
 
